81b5c206763889c90eab0a42e902982000a7e2f7
raidsnail <raidsnail@raidsnail_PC>
2016-06-05 17:00:04 +0200

    added exception classname to output

--- General/Main.java
+++ General/Main.java
@@ -12,11 +12,12 @@ public class Main {
 
 				Calculator calc = new Calculator();
 				
-				calc.setInputStream("4 5~6c ~78 ~9cw");
+				// calc.setInputStream("4 5~6c ~78 ~9cw");
+				calc.setInputStream("4 4+-w");
 				try {
 					calc.executeInput();
 				} catch (Exception e) {
-					System.out.println(e.getMessage()); //TODO: set messages in Operations
+					System.out.println(e.getClass() + ": " + e.getMessage()); //TODO: set messages in Operations
 				}		
 				System.out.println(calc.getOutputStream());
 
